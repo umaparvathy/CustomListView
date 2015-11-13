238a7bb09d4734db882ee503f688c5c9c0b5fb4e
umaparvathy <umavenkee@gmail.com>
2015-11-13 11:44:37 +0530

    Menu options added

--- app/src/main/java/com/uv/customlistview/MainActivity.java
+++ app/src/main/java/com/uv/customlistview/MainActivity.java
@@ -1,14 +1,23 @@
 package com.uv.customlistview;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -41,18 +50,18 @@ public class MainActivity extends AppCompatActivity {
         /*asc = (Button) findViewById(R.id.asc);
         desc = (Button) findViewById(R.id.desc);*/
         contactList = (ListView) findViewById(R.id.contactList);
+        registerForContextMenu(contactList);
         contactsAdapter = new ArrayAdapter<ContactDetails>(getApplicationContext(), R.layout.contact_item, contacts) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
-                if(convertView == null){
+                if (convertView == null) {
                     convertView = getLayoutInflater().inflate(R.layout.contact_item, parent, false);
                 }
                 ContactDetails currentContact = contacts.get(position);
-                TextView contactName = (TextView)convertView.findViewById(R.id.contactName);
-                TextView phoneNumber = (TextView)convertView.findViewById(R.id.contactNumber);
+                TextView contactName = (TextView) convertView.findViewById(R.id.contactName);
+                TextView phoneNumber = (TextView) convertView.findViewById(R.id.contactNumber);
                 contactName.setText(currentContact.getName());
                 phoneNumber.setText(currentContact.getPhoneNumber());
-
                 return convertView;
             }
         };
@@ -76,5 +85,60 @@ public class MainActivity extends AppCompatActivity {
         return contacts;
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Select action");
+        menu.add(1, 1001, 1, "Call");
+        menu.add(1, 1002, 2, "SMS");
+    }
 
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int arrayAdapterPosition = menuInfo.position;
+        ContactDetails selectedContact = contactsAdapter.getItem(arrayAdapterPosition);
+        String number = selectedContact.getPhoneNumber();
+        Intent callIntent;
+        int id = item.getItemId();
+        switch (id) {
+            case 1001:
+                Log.d("MainActivity", "Call option is selected");
+                callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + number));
+
+                /*if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for Activity#requestPermissions for more details.
+                    return false;
+                }*/
+                try {
+                    startActivity(callIntent);
+                } catch(Exception e) {
+                    Log.e("Exception", e.getMessage());
+                }
+                break;
+            case 1002:
+                Log.d("MainActivity", "SMS option is selected");
+                callIntent = new Intent(Intent.ACTION_SENDTO);
+                callIntent.addCategory(Intent.CATEGORY_DEFAULT);
+                callIntent.setType("vnd.android-dir/mms-sms");
+                callIntent.setData(Uri.parse("sms:" + number));
+                try {
+                    startActivity(callIntent);
+                } catch(Exception e) {
+                    Log.e("Exception", e.getMessage());
+                }
+                break;
+        }
+
+        return true;
+    }
 }
